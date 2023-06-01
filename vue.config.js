fb7a2540bb0f7bf2f5ef39e1e5e780b12ef46fa5
Eilly56 <18382065209@163.com>
2023-06-01 10:30:48 +0800

    wu

--- vue.config.js
+++ vue.config.js
@@ -83,7 +83,29 @@ module.exports = {
 			threshold: 10240,
 			minRatio: 0.8
 		  })
-		]
+		],
+		// 开启分离 js
+		optimization: {
+		  runtimeChunk: 'single',
+		  splitChunks: {
+			chunks: 'all',
+			maxInitialRequests: Infinity,
+			minSize: 20000,
+			cacheGroups: {
+			  vendor: {
+				test: /[\\/]node_modules[\\/]/,
+				name (module) {
+				  // get the name. E.g. node_modules/packageName/not/this/part.js
+				  // or node_modules/packageName
+				  const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+				  // npm package names are URL-safe, but some servers don't like @ symbols
+				  return `npm.${packageName.replace('@', '')}`
+				}
+			  }
+			}
+		  }
+		}
 	  }
+	
 
 }
