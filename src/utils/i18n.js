90b7c13e3077892ad6e1140e8b4cb0992cd5de08
HtetKo510217 <htetko510217@gmail.com>
2024-03-27 14:42:11 +0700

    Add translation support for Malaysia, Thailand, and Vietnam

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueI18n from "vue-i18n";
 import en from "../assets/locales/en.json";
 import zh from "../assets/locales/zh.json";
+import th from "../assets/locales/th.json";
+import vi from "../assets/locales/vi.json";
+import ms from "../assets/locales/ms.json";
 
 Vue.use(VueI18n)
 export default new VueI18n({
@@ -10,6 +13,9 @@ export default new VueI18n({
     localeDir: "locales",
     messages: {
         en: en,
-        zh: zh
+        zh: zh,
+        th: th,
+        vi: vi,
+        ms: ms,
     }
 })
