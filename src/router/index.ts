47f04555d918835fca0417259116f60fd4ea6d03
Angel <sniper62899@gmail.com>
2025-09-30 13:30:36 +0800

    updated

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import { useAppStore } from "@/store";
 import { removeToken } from "@/utils/auth";
 // routes
@@ -68,7 +68,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
